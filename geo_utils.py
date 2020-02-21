291ff7ef4e618eed008b762b33775bf3e7399b7f
Aleksander Hulist <ahulist@fp.lan@FP-PC3419.fp.lan>
2020-02-21 12:13:11 +0100

    added eccentricity

--- geo_utils.py
+++ geo_utils.py
@@ -8,6 +8,7 @@ def get_diam_phys_parametrized(data, value, spacing):
     :param value: value the area is marked with
     :param spacing:
     :return: physical diameter
+    :return: eccentricity [0-1] where 0 is a circle
     """
     coords = [(np.min(dim), np.max(dim) + 1) for dim in np.where(data == value)]
     if np.sum([1 == (max_ - min_) for min_, max_ in coords]) != 1:
@@ -15,4 +16,5 @@ def get_diam_phys_parametrized(data, value, spacing):
     inflow = np.squeeze(data[coords[0][0]:coords[0][1], coords[1][0]:coords[1][1], coords[2][0]:coords[2][1]])
 
     from skimage.measure import regionprops
-    return regionprops(inflow)[0].equivalent_diameter * spacing
+    props = regionprops(inflow)[0]
+    return props.equivalent_diameter * spacing, props.eccentricity
